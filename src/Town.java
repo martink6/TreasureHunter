ff023efed2db612c01426c4b13c24c8c7f7f54f3
BT_2N2_25 <sample@bths.edu>
2023-12-20 13:27:28 -0500

    added marsh and boots

--- src/Town.java
+++ src/Town.java
@@ -124,16 +124,18 @@ public class Town {
      */
     private Terrain getNewTerrain() {
         double rnd = Math.random();
-        if (rnd < .2) {
+        if (rnd < .166) {
             return new Terrain("Mountains", "Rope");
-        } else if (rnd < .4) {
+        } else if (rnd < .333) {
             return new Terrain("Ocean", "Boat");
-        } else if (rnd < .6) {
+        } else if (rnd < .5) {
             return new Terrain("Plains", "Horse");
-        } else if (rnd < .8) {
+        } else if (rnd < .667) {
             return new Terrain("Desert", "Water");
-        } else {
+        } else if (rnd < .833) {
             return new Terrain("Jungle", "Machete");
+        } else {
+            return new Terrain("Marsh", "Boots");
         }
     }
 
